a57e55d8dfdb91bf5f76f54554c0eea30f837276
Serhii Shevchenko <serhii.shevchenko@gmail.com>
2022-05-20 14:12:03 +0300

    feat: resolve 04 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+import { readdir, copyFile, stat, mkdir } from 'fs/promises'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export const copyFiles = async (root, copy) => {
+  // create folder if not exists
+  await mkdir(`${copy}`, { recursive: true })
+
+  // read files
+  const files = await readdir(root, { withFileTypes: true })
+  files.forEach(async (file) => {
+    const filePath = `${root}/${file.name}`
+    const copyPath = `${copy}/${file.name}`
+
+    const fileStats = await stat(filePath).catch((err) => {
+      throw new Error(err)
+    })
+
+    // if file is directory, copy files recursively
+    if (fileStats.isDirectory()) await copyFiles(filePath, copyPath)
+    else await copyFile(filePath, copyPath)
+  })
+}
+
+// task 4
+copyFiles(`${__dirname}/files`, `${__dirname}/copy-files`)
